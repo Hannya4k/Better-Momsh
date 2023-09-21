950e3de860ab76d97b3bb2b2380f760552b4aea7
Karl <karlthegamer24@gmail.com>
2023-09-22 02:43:32 +0800

    dashboard and api folder

--- src/screens/Dashboard.tsx
+++ src/screens/Dashboard.tsx
@@ -1,16 +1,20 @@
-import { ScrollView, StyleSheet, Text, View, TextInput, Image } from 'react-native'
+import { ScrollView, StyleSheet, Text, View, TextInput, Image, TouchableOpacity } from 'react-native'
 import React, { useState } from 'react';
 import commonStyles from '../components/commonStyles'
-import { BottomTabBar } from '@react-navigation/bottom-tabs';
 
 
 const Dashboard = () => {
+    const [selectedTouchable, setSelectedTouchable] = useState(null);
 
+    const handleTouchablePress = (touchableName) => {
+        setSelectedTouchable(touchableName);
+    };
 
     return (
         <ScrollView style={styles.container}>
             <View style={commonStyles.container1}>
-                <Image source={require('../../assets/bg-dashboard.png')} style={styles.bg} />
+
+                {/* <Image source={require('../../assets/bg-dashboard.png')} style={styles.bg} /> */}
                 <TextInput
                     style={styles.searchBar}
                     placeholder="Search"
@@ -21,9 +25,41 @@ const Dashboard = () => {
                     <Text style={styles.text}>Search for your OB-GYN and book an
                         appointment now.</Text>
                 </View>
+
+                <View style={styles.btnContainer}>
+                    <TouchableOpacity onPress={() => handleTouchablePress('Doctors')}
+                        style={[
+                            styles.btn,
+                            selectedTouchable === 'Doctors' && styles.clickedBtn,
+                        ]}
+                    >
+                        <Text style={styles.btnText}>Doctors</Text>
+                    </TouchableOpacity >
+
+                    <TouchableOpacity onPress={() => handleTouchablePress('Maternity Clinics')}
+                        style={[
+                            styles.btn,
+                            selectedTouchable === 'Maternity Clinics' && styles.clickedBtn,
+                        ]}>
+                        <Text style={styles.btnText}>Maternity Clinics</Text>
+                    </TouchableOpacity>
+                </View>
+
+                <View style={styles.placeholder}>
+                    <Text>test</Text>
+                </View>
+
+                <View style={styles.placeholder}>
+                    <Text>test</Text>
+                </View>
+
+                <View style={styles.placeholder}>
+                    <Text>test</Text>
+                </View>
+
+                <Text style={styles.headerText}>Recent Appointment</Text>
+
             </View>
-
-
         </ScrollView>
 
     )
@@ -34,12 +70,12 @@ export default Dashboard
 const styles = StyleSheet.create({
     bg: {
         position: 'absolute',
-        left: -120,
-        bottom: 0,
+        left: 0,
+        bottom: 0
     },
 
     container: {
-        
+
         height: 600,
     },
     searchBar: {
@@ -69,5 +105,30 @@ const styles = StyleSheet.create({
         color: 'white',
         fontSize: 16,
         fontFamily: 'Lato-Regular'
+    },
+    btnContainer: {
+        flexDirection: 'row',
+        margin: 10
+    },
+    btn: {
+        margin: 40,
+        marginBottom: 10,
+    },
+    btnText: {
+        fontSize: 18,
+        fontWeight: '600',
+        color: '#fff',
+        fontFamily: 'Lato-Bold'
+    },
+    clickedBtn: {
+        borderBottomWidth: 2, 
+        borderBottomColor: '#FDC380',
+    },
+    placeholder: {
+        width: 330,
+        height: 69,
+        backgroundColor: 'white',
+        marginBottom: 30,
+        borderRadius: 20
     }
 })
